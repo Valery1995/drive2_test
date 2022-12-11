48db127ee501e5c00e71b22d567dc171355f4d57
Valery Aniskevich <aniskevich-2018@mail.ru>
2022-12-12 00:39:36 +0300

    diplom save 12.12 00:39

--- pages/market_page.py
+++ pages/market_page.py
@@ -2,11 +2,18 @@ from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+from selenium.webdriver.support.ui import Select
 
 all_subcategories = (By.CSS_SELECTOR, 'a[class="market-categories__subcategory c-link c-link--text"]')
 city_list = (By.CSS_SELECTOR, 'a[class="c-side-nav__link c-link c-link--text js-side-link"]')
 car_details = (By.CSS_SELECTOR, 'a[class="u-link-area"]')
 location_information = (By.TAG_NAME, 'td')
+condition_details = (By.CSS_SELECTOR, 'label[class="c-side-nav__item "]')
+
+select_brand = (By.CSS_SELECTOR, 'select[name="brand"]')
+select_model = (By.CSS_SELECTOR, 'select[name="model"]')
+categories_details = (By.CSS_SELECTOR, 'label[class="c-side-nav__item"]')
+information = (By.TAG_NAME, 'tbody')
 
 
 class MarketPage(BasePage):
@@ -15,6 +22,9 @@ class MarketPage(BasePage):
 
     def open_page(self):
         self.driver.get(f'{self.base_url}/market')
+
+    def open_page_filter(self):
+        self.driver.get(f'{self.base_url}/market?location=2191')
 
     def all_subcategories_is_enabled(self):
         subcategories = self.find_elements(all_subcategories)
@@ -40,3 +50,35 @@ class MarketPage(BasePage):
                     return [False, f'В фильтре "{name_city}" деталь из города {location}']
                 self.driver.back()
         return True, ''
+
+    def categories_details_is_enabled(self):
+        categories = self.find_elements(categories_details)
+        for i in categories:
+            result = bool(i.is_enabled())
+            if result == False:
+                return [False, f'category "{i.text}" not enabled']
+        return True, ''
+
+    def new_detail_filter_is_ok(self):
+        self.find_elements(condition_details)[0].click()
+        sleep(1)  # нужен слип потому что не успевает кликнуть фильтр состояние
+        for i in range(10):
+            details = self.find_elements(car_details)
+            details[i].click()
+            condition = self.find_elements(information)[0].text.find('Состояние новое')
+            if condition == -1:
+                return False
+            self.driver.back()
+        return True
+
+    def old_detail_filter_is_ok(self):
+        self.find_elements(condition_details)[1].click()
+        sleep(1)  # нужен слип потому что не успевает кликнуть фильтр состояние
+        for i in range(10):
+            details = self.find_elements(car_details)
+            details[i].click()
+            condition = self.find_elements(information)[0].text.find('Состояние б/у')
+            if condition == -1:
+                return False
+            self.driver.back()
+        return True
--- tests/test_market.py
+++ tests/test_market.py
@@ -1,9 +1,9 @@
 from pages.market_page import MarketPage
 from time import sleep
 import pytest
+from selenium.webdriver.support.ui import Select
 
 
-# @pytest.mark.parametrize('creds', all_subcategories_is_enabled())
 def test_all_subcategories_is_enabled(driver):
     market = MarketPage(driver)
     market.open_page()
@@ -15,3 +15,20 @@ def test_location_filter(driver):
     market.open_page()
     assert market.correct_location_filter()[0], market.correct_location_filter()[1]
 
+
+def test_categories_is_enabled(driver):
+    market = MarketPage(driver)
+    market.open_page_filter()
+    assert market.categories_details_is_enabled()[0], market.categories_details_is_enabled()[1]
+
+
+def test_new_detail_filter(driver):
+    condition = MarketPage(driver)
+    condition.open_page_filter()
+    assert condition.new_detail_filter_is_ok()
+
+
+def test_old_detail_filter(driver):
+    condition = MarketPage(driver)
+    condition.open_page_filter()
+    assert condition.old_detail_filter_is_ok()
