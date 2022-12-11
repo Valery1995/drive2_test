381c521cc606efdc950840604b9299441a5b1db8
Valery Aniskevich <aniskevich-2018@mail.ru>
2022-12-11 20:27:04 +0300

    diplom save 11.12 20:26

--- pages/market_page.py
+++ pages/market_page.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 all_subcategories = (By.CSS_SELECTOR, 'a[class="market-categories__subcategory c-link c-link--text"]')
+city_list = (By.CSS_SELECTOR, 'a[class="c-side-nav__link c-link c-link--text js-side-link"]')
+car_details = (By.CSS_SELECTOR, 'a[class="u-link-area"]')
+location_information = (By.TAG_NAME, 'td')
 
 
 class MarketPage(BasePage):
@@ -12,16 +15,28 @@ class MarketPage(BasePage):
 
     def open_page(self):
         self.driver.get(f'{self.base_url}/market')
-        sleep(2)
 
     def all_subcategories_is_enabled(self):
         subcategories = self.find_elements(all_subcategories)
-        for i in range(len(subcategories)):
-            result = bool(subcategories[i].is_enabled())
+        for i in subcategories:
+            result = bool(i.is_enabled())
             if result == False:
-                print(f'link №{i+1} not enabled')
-                return False
-        return True
+                return [False, f'link "{i.text}" not enabled']
+        return True, ''
 
-
-
+    def correct_location_filter(self):
+        cites = self.find_elements(city_list)
+        for i in range(len(cites)):
+            self.driver.get(f'{self.base_url}/market')
+            cites = self.find_elements(city_list)
+            name_city = cites[i].text
+            cites[i].click()
+            for num in range(1):
+                details = self.find_elements(car_details)
+                details[num].click()
+                location = self.find_elements(location_information)[0].text.split(' (')[0]
+                result = bool(name_city == location)
+                if result == False:
+                    return [False, f'В фильтре "{name_city}" деталь из города {location}']
+                self.driver.back()
+        return True, ''
--- tests/test_market.py
+++ tests/test_market.py
@@ -7,9 +7,11 @@ import pytest
 def test_all_subcategories_is_enabled(driver):
     market = MarketPage(driver)
     market.open_page()
-    assert market.all_subcategories_is_enabled()
+    assert market.all_subcategories_is_enabled()[0], market.all_subcategories_is_enabled()[1]
 
 
-
-
+def test_location_filter(driver):
+    market = MarketPage(driver)
+    market.open_page()
+    assert market.correct_location_filter()[0], market.correct_location_filter()[1]
 
